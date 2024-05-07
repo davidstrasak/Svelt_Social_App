d01b7587691a6dec5a683941aaf2d5c0f2b928eb
David Strasak <david.strasak@honeywell.com>
2024-05-07 14:10:28 +0200

    add fibonacci sequence

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	console.log('hello world');
+</script>
+
+<h1 class="text-3xl font-bold underline text-red-700 first-letter:text-red-500">Hello world!</h1>
---
+++ src/routes/task-manager/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let count = 1;
+
+	let text = ['1', '1'];
+
+	onMount(() => {
+		if (typeof document !== 'undefined') {
+			document.addEventListener('keydown', (e) => {
+				if (e.key === 'Enter') {
+					handleClick();
+				}
+			});
+		}
+	});
+
+	function handleClick() {
+		count += count;
+		text.push(count.toString());
+	}
+
+	let arrays = text.map((item) => {
+		return item;
+	});
+
+	$: {
+		count;
+		arrays = text.map((item) => {
+			return item;
+		});
+	}
+</script>
+
+<div class="text-white">
+	<div>This is the fibonacci sequence - {count}</div>
+	<button
+		on:click={() => {
+			return handleClick();
+		}}>Increment</button
+	>
+	<article class="w-1 text-pretty relative">
+		<div>{arrays.join(' ')}</div>
+	</article>
+</div>
