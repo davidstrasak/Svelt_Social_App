fccdf62e59d48a7f0212f727174ae276d85e9059
David Strasak <david.strasak@honeywell.com>
2024-05-15 10:26:16 +0200

    format code

--- src/routes/fibonacci/+layout.svelte
+++ src/routes/fibonacci/+layout.svelte
@@ -1,3 +1,3 @@
 <div class="p-20">
-	<slot />
+  <slot />
 </div>
--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,18 +1,18 @@
 <script>
-	import TaskList from '$lib/task-manager/TaskList.svelte';
-	const listName = 'Honeywell';
+  import TaskList from "$lib/task-manager/TaskList.svelte";
+  const listName = "Honeywell";
 </script>
 
 <div class="p-10 h-full">
-	<div class="text-white text-2xl mb-6">Some List</div>
-	<button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
-		+ Add List
-	</button>
-	<div class="flex-it h-full">
-		<div class="flex-it flex-row rounded-xl h-full">
-			<TaskList {listName} />
-			<TaskList listName="BUT" />
-			<TaskList listName="life" />
-		</div>
-	</div>
+  <div class="text-white text-2xl mb-6">Some List</div>
+  <button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
+    + Add List
+  </button>
+  <div class="flex-it h-full">
+    <div class="flex-it flex-row rounded-xl h-full">
+      <TaskList {listName} />
+      <TaskList listName="BUT" />
+      <TaskList listName="life" />
+    </div>
+  </div>
 </div>
