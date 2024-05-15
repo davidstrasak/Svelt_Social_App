44207bea24cd687a2463512c6061f0dd172bf18e
David Strasak <david.strasak@honeywell.com>
2024-05-15 13:15:31 +0200

    add text editing binds

---
+++ src/lib/task-manager/Editable.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let value;
+  let isEditing = false;
+</script>
+
+{#if isEditing}
+  <div class="flex-it">
+    <textarea bind:value />
+    <button
+      class="flex underline"
+      on:click={() => {
+        isEditing = false;
+      }}>Close</button
+    >
+  </div>
+{:else}
+  <button
+    class="block"
+    on:click={() => {
+      isEditing = true;
+    }}
+  >
+    <slot />
+  </button>
+{/if}
