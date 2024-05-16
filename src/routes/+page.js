9a32e40eeb25b032c262bef18a08a4e3361bfae9
davidstrasak <davidstrasak@seznam.cz>
2024-05-16 19:25:00 +0200

    configure client side rendering

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,8 @@
 export const ssr = false;
 
 export function load() {
-  console.log("Hello from +page.js");
-  return { test1: "Hello world", test2: 1000 };
+  const randomNum = Math.floor(Math.random() * 10);
+  console.log("Random number: ", randomNum);
+
+  return { test1: "Hello world", test2: randomNum };
 }
